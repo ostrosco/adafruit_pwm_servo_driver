066fe52708fdf0453e82525adde364039276506e
ostrosco <ostrosco@fastmail.fm>
2017-05-04 21:58:44 -0600

    - added code to set the PWM frequency

--- src/servo_driver.rs
+++ src/servo_driver.rs
@@ -44,6 +44,7 @@ impl PCA9685 {
     }
 
     pub fn init(&mut self) -> Result<(), LinuxI2CError> {
+        try!(self.set_all_pwm(0, 0));
         try!(self.device.smbus_write_byte_data(MODE2, OUTDRV));
         try!(self.device.smbus_write_byte_data(MODE1, ALLCALL));
         sleep(time::Duration::from_millis(5));
@@ -52,6 +53,29 @@ impl PCA9685 {
         let mode1 = mode1 & !SLEEP;
         try!(self.device.smbus_write_byte_data(MODE1, mode1));
         sleep(time::Duration::from_millis(5));
+        Ok(())
+    }
+
+    pub fn set_pwm_freq(&mut self, freq_hz: f32) -> Result<(), LinuxI2CError> {
+        let prescaleval = 25e6 / 4096.0 / freq_hz - 1.0;
+        let prescale = (prescaleval + 0.5).floor() as u8;
+        
+        // Grab the old mode so we can revert our mode changes once we're done
+        // setting the prescale.
+        let old_mode = try!(self.device.smbus_read_byte_data(MODE1));
+
+        // This new mode is used to put the device to sleep so we can set the
+        // prescaler safely.
+        let new_mode = old_mode & 0x7F | SLEEP;
+        
+        // Set the mode to sleep, set the prescaler, and wake up.
+        try!(self.device.smbus_write_byte_data(MODE1, new_mode)); 
+        try!(self.device.smbus_write_byte_data(PRESCALE, prescale));
+        try!(self.device.smbus_write_byte_data(MODE1, old_mode));
+        sleep(time::Duration::from_secs(5));
+        
+        // We need to turn on auto-incrememt for transmission to work properly.
+        try!(self.device.smbus_write_byte_data(MODE1, old_mode | 0xA1));
         Ok(())
     }
 
